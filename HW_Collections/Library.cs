b8509cd10c2110af59c0106e858f558ad35115d4
dmitriy1024 <dfedoryshyn@gmail.com>
2015-08-29 22:52:55 +0300

    HW_Collections
    HW_Collections

---
+++ HW_Collections/Library.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Threading;
+
+namespace HW_Collections
+{
+    class Library
+    {
+        private ObservableCollection<Book> _books = new ObservableCollection<Book>();
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged
+        {
+            add
+            {
+                lock(_books)
+                {
+                    _books.CollectionChanged += value;
+                }
+            }
+            remove
+            {   
+                lock(_books)
+                {
+                    _books.CollectionChanged -= value;
+                }                
+            }
+        }
+
+        public void AddHandler(NotifyCollectionChangedEventHandler handler)
+        {
+            if(handler != null)
+            {
+                _books.CollectionChanged += handler;
+            }
+        }
+
+        public void RemoveHandler(NotifyCollectionChangedEventHandler handler)
+        {
+            if (handler != null)
+            {
+                _books.CollectionChanged -= handler;
+            }
+        }
+
+        public void AddBook(Book book)
+        {
+            if (book == null)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                _books.Add(book);
+            }
+        }
+
+        public Book GetBook(string title)
+        {
+            if (title == null)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                Book findedBook = null;
+                foreach (var item in _books)
+                {
+                    if (String.Compare(item.Title, title, true) == 0)
+                    {
+                        findedBook = item;
+                    }
+                }
+
+                if (findedBook != null)
+                {
+                    _books.Remove(findedBook);
+                }
+
+                return findedBook;
+            }
+        }
+    }
+}
---
+++ HW_Collections/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace HW_Collections
+{
+    class Student
+    {
+        private string _name;
+        private Genre _preferedGenre;
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public Genre PreferedGenre
+        {
+            get { return _preferedGenre; }
+        }
+
+        public Student(string name, Genre prefGenre, Library lib)
+        {
+            if (name == null || lib == null)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                _name = name;
+                _preferedGenre = prefGenre;
+
+                lib.CollectionChanged += LibChanged;
+            }
+        }
+
+        private void LibChanged(object o, NotifyCollectionChangedEventArgs args)
+        {
+            if(args.Action == NotifyCollectionChangedAction.Add)
+            {
+                if (((Book)args.NewItems[0]).Genre == Genre.Computer && _preferedGenre == Genre.Computer)
+                {
+                    Console.WriteLine("{0}: I'm going to go to the library. Trere is a new computer book!", _name);
+                }
+
+                if (((Book)args.NewItems[0]).Genre == Genre.Fantastic && _preferedGenre == Genre.Fantastic)
+                {
+                    Console.WriteLine("{0}: I want a home delivery of a new fantastic book!", _name);
+                }
+            }
+            if(args.Action == NotifyCollectionChangedAction.Remove)
+            {
+                if (_preferedGenre == Genre.Fantastic && ((Book)args.OldItems[0]).Genre != Genre.Fantastic)
+                {
+                    Console.WriteLine("{0}: I'm not going to read \"{1}\"", _name, ((Book)args.OldItems[0]).Title);
+                }
+            }
+        }
+    }
+}
